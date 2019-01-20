065a26e674ca1bc0f5af413e8a833ea719963d9b
Pavle Jonoski <jonoski.pavle@gmail.com>
2019-01-20 02:46:47 +0100

    Initial watching of file changes.

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/theia-log/selene/watcher"
 )
 
 func main() {
 	fmt.Println("test")
+
+	wd := watcher.NewWatchDaemon()
+
+	wd.Start()
+
+	src := watcher.NewFileSource("./tmp/test")
+
+	src.OnSourceEvent(func(file string, diff []byte) {
+		fmt.Println(file, " -> ", string(diff))
+	})
+	wd.AddSource("ft", src)
+
+	fmt.Println("started..")
+	fmt.Scanf("L")
+	wd.Stop()
 }
--- watcher/fsnotify.go
+++ watcher/fsnotify.go
@@ -2,6 +2,10 @@ package watcher
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/fsnotify/fsnotify"
@@ -9,21 +13,106 @@ import (
 
 type FSNotifyEventSource struct {
 	*GenericEventSource
+	AbsFilePath string
+	ParentDir   string
+	currentPos  int64
 }
 
 func (s *FSNotifyEventSource) AttachToWatcher(watcher *fsnotify.Watcher) error {
-	return watcher.Add(s.FilePath)
+	return watcher.Add(s.ParentDir)
 }
 
 func (s *FSNotifyEventSource) DetachFromWatcher(watcher *fsnotify.Watcher) error {
-	return watcher.Remove(s.FilePath)
+	return watcher.Remove(s.ParentDir)
+}
+
+func (s *FSNotifyEventSource) fileAvailable() error {
+	f, err := os.Open(s.AbsFilePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	endPos, err := f.Seek(0, os.SEEK_END)
+	if err != nil {
+		return err
+	}
+	s.currentPos = endPos
+	return nil
+}
+
+func (s *FSNotifyEventSource) calculateDiff() ([]byte, error) {
+	f, err := os.Open(s.AbsFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	curr := s.currentPos
+
+	_, err = f.Seek(curr, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+
+	diff, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	curr, err = f.Seek(0, os.SEEK_CUR)
+	if err != nil {
+		return nil, err
+	}
+
+	s.currentPos = curr
+
+	return diff, err
+}
+
+func (s *FSNotifyEventSource) handleFSNotifyEvent(ev fsnotify.Event) error {
+	if ev.Op&fsnotify.Write == fsnotify.Write {
+		diff, err := s.calculateDiff()
+		if err != nil {
+			return err
+		}
+		s.Trigger(diff)
+	} else if ev.Op&fsnotify.Create == fsnotify.Create {
+		if err := s.fileAvailable(); err != nil {
+			return err
+		}
+		diff, err := s.calculateDiff()
+		if err != nil {
+			return err
+		}
+		s.Trigger(diff)
+	}
+	return nil
+}
+
+func NewFileSource(filePath string) EventSource {
+	parentDir, absPath, err := GetFileParts(filePath)
+	if err != nil {
+		panic(err) // FIXME
+	}
+
+	fileSource := &FSNotifyEventSource{
+		GenericEventSource: NewEventSource(filePath),
+		AbsFilePath:        absPath,
+		ParentDir:          parentDir,
+	}
+
+	fileSource.fileAvailable()
+
+	return fileSource
 }
 
 type FSNotifyWatcher struct {
-	watcher *fsnotify.Watcher
-	sources map[string]EventSource
-	started bool
-	mux     sync.Mutex
+	watcher     *fsnotify.Watcher
+	watchedDirs map[string][]EventSource
+	sources     map[string]EventSource
+	started     bool
+	mux         sync.Mutex
+	done        chan bool
 }
 
 func (f *FSNotifyWatcher) Start() error {
@@ -33,6 +122,7 @@ func (f *FSNotifyWatcher) Start() error {
 		return fmt.Errorf("already started")
 	}
 	f.started = true
+	f.listenForChanges()
 	return nil
 }
 
@@ -43,6 +133,7 @@ func (f *FSNotifyWatcher) Stop() error {
 		return fmt.Errorf("stopped")
 	}
 	f.watcher.Close()
+	<-f.done
 	return nil
 }
 
@@ -54,14 +145,19 @@ func (f *FSNotifyWatcher) AddSource(source string, eventSource EventSource) (Eve
 		return src, nil
 	}
 	if fsnSource, ok := eventSource.(*FSNotifyEventSource); ok {
-		if err := fsnSource.AttachToWatcher(f.watcher); err != nil {
-			f.mux.Unlock()
-			return nil, err
+		sourcesInDir, ok := f.watchedDirs[fsnSource.ParentDir]
+		if !ok {
+			sourcesInDir = []EventSource{}
+			if err := fsnSource.AttachToWatcher(f.watcher); err != nil {
+				f.mux.Unlock()
+				return nil, err
+			}
 		}
+		sourcesInDir = append(sourcesInDir, fsnSource)
+		f.watchedDirs[fsnSource.ParentDir] = sourcesInDir
 	}
 	f.sources[source] = eventSource
 	f.mux.Unlock()
-
 	return eventSource, nil
 }
 
@@ -85,4 +181,73 @@ func (f *FSNotifyWatcher) RemoveSource(source string) error {
 	// TODO: generic dispose call
 
 	return nil
+}
+
+func (f *FSNotifyWatcher) listenForChanges() {
+	go func() {
+		defer func() {
+			f.done <- true
+		}()
+		for {
+			select {
+			case event, ok := <-f.watcher.Events:
+				if !ok {
+					return
+				}
+				f.handleWatcherEvent(event)
+			case err, ok := <-f.watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("[ERR]: Watcher event error: ", err.Error())
+			}
+		}
+	}()
+}
+
+func (f *FSNotifyWatcher) handleWatcherEvent(ev fsnotify.Event) {
+	parentDir, absPath, err := GetFileParts(ev.Name)
+	if err != nil {
+		log.Println("error: cannot determine abs path for event: ", ev.Name)
+		return
+	}
+	sources, ok := f.watchedDirs[parentDir]
+	if !ok {
+		return
+	}
+
+	for _, source := range sources {
+		fsource := source.(*FSNotifyEventSource)
+		if fsource.AbsFilePath == absPath {
+			go func() {
+				if err = fsource.handleFSNotifyEvent(ev); err != nil {
+					log.Println("Error in handling event: ", err.Error())
+				}
+			}()
+		}
+	}
+}
+
+func NewWatchDaemon() WatchDaemon {
+	fsWatcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err) // FIME:
+	}
+	return &FSNotifyWatcher{
+		mux:         sync.Mutex{},
+		sources:     map[string]EventSource{},
+		watchedDirs: map[string][]EventSource{},
+		started:     false,
+		watcher:     fsWatcher,
+		done:        make(chan bool),
+	}
+}
+
+func GetFileParts(filePath string) (parentDir string, absPath string, err error) {
+	absPath, err = filepath.Abs(filePath)
+	if err != nil {
+		return
+	}
+	parentDir = filepath.Dir(absPath)
+	return
 }
