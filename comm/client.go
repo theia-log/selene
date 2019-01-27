5763978ace76f7a58e421ba182fa6d0530f61ff5
Pavle Jonoski <jonoski.pavle@gmail.com>
2019-01-27 02:26:45 +0100

    Websocket based client.

--- comm/client.go
+++ comm/client.go
@@ -5,8 +5,17 @@ import "github.com/theia-log/selene/model"
 type EventFilter struct {
 }
 
+func (f *EventFilter) DumpBytes() ([]byte, error) {
+	return nil, nil
+}
+
+type EventResponse struct {
+	Event *model.Event
+	Error error
+}
+
 type Client interface {
 	Send(event model.Event) error
-	Receive(filter EventFilter) (chan model.Event, error)
-	Find(filter EventFilter) (chan model.Event, error)
+	Receive(filter *EventFilter) (chan *EventResponse, error)
+	Find(filter *EventFilter) (chan *EventResponse, error)
 }
--- comm/websocket.go
+++ comm/websocket.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gorilla/websocket"
+	"github.com/theia-log/selene/model"
 )
 
 type theiaData struct {
@@ -13,11 +14,12 @@ type theiaData struct {
 }
 
 type theiaConn struct {
+	url  string
 	conn *websocket.Conn
 }
 
-func (t *theiaConn) Open(url string) error {
-	c, _, err := websocket.DefaultDialer.Dial(url, nil)
+func (t *theiaConn) Open() error {
+	c, _, err := websocket.DefaultDialer.Dial(t.url, nil)
 	if err != nil {
 		return err
 	}
@@ -64,7 +66,95 @@ func (t *theiaConn) Close(reason string) error {
 		websocket.FormatCloseMessage(websocket.CloseNormalClosure, reason))
 }
 
+func NewConn(baseUrl, action string) *theiaConn {
+	return &theiaConn{
+		url: fmt.Sprintf("%s/%s", baseUrl, action),
+	}
+}
+
 type WebsocketClient struct {
 	baseURL     string
 	connections map[string]*theiaConn
+}
+
+func (w *WebsocketClient) getConn(endpoint string) (*theiaConn, error) {
+	conn, ok := w.connections[endpoint]
+	if !ok {
+		return nil, fmt.Errorf("no such endpoint")
+	}
+	if conn.conn == nil {
+		if err := conn.Open(); err != nil {
+			return nil, err
+		}
+	}
+	return conn, nil
+}
+
+func (w *WebsocketClient) Send(event *model.Event) error {
+	conn, err := w.getConn("event")
+	if err != nil {
+		return err
+	}
+	data, err := event.DumpBytes()
+	if err != nil {
+		return err
+	}
+	return conn.Send(data)
+}
+
+func (w *WebsocketClient) doReceive(endpoint string, filter *EventFilter) (chan *EventResponse, error) {
+	conn, err := w.getConn(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	filterData, err := filter.DumpBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	if err = conn.Send(filterData); err != nil {
+		return nil, err
+	}
+
+	dataChan := conn.Read()
+	eventChan := make(chan *EventResponse)
+
+	go func() {
+		for {
+			data, ok := <-dataChan
+			if !ok {
+				// channel closed
+				close(eventChan)
+				break
+			}
+			if data.err != nil {
+				eventChan <- &EventResponse{
+					Error: data.err,
+				}
+				continue
+			}
+			ev := &model.Event{}
+			if err = ev.LoadBytes(data.data); err != nil {
+				eventChan <- &EventResponse{
+					Error: err,
+				}
+				continue
+			}
+
+			eventChan <- &EventResponse{
+				Event: ev,
+			}
+		}
+	}()
+
+	return eventChan, nil
+}
+
+func (w *WebsocketClient) Receive(filter *EventFilter) (chan *EventResponse, error) {
+	return w.doReceive("live", filter)
+}
+
+func (w WebsocketClient) Find(filter *EventFilter) (chan *EventResponse, error) {
+	return w.doReceive("find", filter)
 }
