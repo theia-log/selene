5d19ceeb1268a2f0322a5969aceb183b29328fa9
Pavle Jonoski <jonoski.pavle@gmail.com>
2019-01-31 02:34:40 +0100

    CLI flags and flag sets.

--- cli/parser.go
+++ cli/parser.go
@@ -9,11 +9,38 @@ type GlobalFlags struct {
 	Port      *int
 }
 
+type QueryFlags struct {
+}
+
+type WatcherFlags struct {
+}
+
 func SetupGlobalFlags() *GlobalFlags {
 	gf := &GlobalFlags{}
 
 	gf.ServerURL = flag.String("server", "ws://localhost:6433", "Theia server URL.")
 	gf.Verbose = flag.Bool("v", false, "Verbose output")
+	gf.Host = flag.String("H", "", "Theia host")
+	gf.Port = flag.Int("p", 0, "Theia port")
+
+	return gf
+}
+
+func SetupQueryFlags() (*QueryFlags, *flag.FlagSet) {
+	return nil, nil
+}
+
+func SetupWatcherFlags() (*QueryFlags, *flag.FlagSet) {
+	return nil, nil
+}
+
+func SetupGlobalFlagsOn(fg *flag.FlagSet) *GlobalFlags {
+	gf := &GlobalFlags{}
+
+	gf.ServerURL = fg.String("server", "ws://localhost:6433", "Theia server URL.")
+	gf.Verbose = fg.Bool("v", false, "Verbose output")
+	gf.Host = fg.String("H", "", "Theia host")
+	gf.Port = fg.Int("p", 0, "Theia port")
 
 	return gf
 }
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/theia-log/selene/cli"
 )
 
 func main() {
-	args := cli.SetupGlobalFlags()
+	cli.SetupGlobalFlags()
 	flag.Parse()
-	fmt.Printf("args=%v\n", args)
 }
