540b5bf7db79e9e080fb9f4b4a8be6ece94848d7
Pavle Jonoski <jonoski.pavle@gmail.com>
2019-03-03 21:05:57 +0100

    Websocket mock test

--- comm/websocket_test.go
+++ comm/websocket_test.go
@@ -2,7 +2,6 @@ package comm
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -13,39 +12,88 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type mockTransport struct {
+type Message []byte
+
+func (m Message) EqualsTo(message []byte) bool {
+	return string(m) == string(message)
 }
 
-func (m *mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	return nil, fmt.Errorf("error:test")
+type websocketMock struct {
+	expect   Message
+	respond  []Message
+	MockURL  string
+	upgrader websocket.Upgrader
+	Errors   []error
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+func (w *websocketMock) Expect(mesage string) *websocketMock {
+	w.expect = []byte(mesage)
+	return w
 }
 
-func websocketHandler(w http.ResponseWriter, req *http.Request) {
-	conn, err := upgrader.Upgrade(w, req, nil)
+func (w *websocketMock) Respond(message string) *websocketMock {
+	w.respond = append(w.respond, []byte(message))
+	return w
+}
+
+func (w websocketMock) upgradedHandler(resp http.ResponseWriter, req *http.Request) {
+	conn, err := w.upgrader.Upgrade(resp, req, nil)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("cannot upgrade: %v", err), http.StatusInternalServerError)
+		http.Error(resp, fmt.Sprintf("cannot upgrade: %v", err), http.StatusInternalServerError)
 	}
 	mt, p, err := conn.ReadMessage()
 	if err != nil {
-		log.Printf("cannot read message: %v", err)
+		w.Errors = append(w.Errors, err)
 		return
 	}
-	conn.WriteMessage(mt, []byte("hello "+string(p)))
+
+	if w.expect != nil {
+		if !w.expect.EqualsTo(p) {
+			w.Errors = append(w.Errors, fmt.Errorf("expected '%s' but got '%s'", string(w.expect), string(p)))
+			return
+		}
+	}
+
+	if w.respond != nil {
+		for _, msg := range w.respond {
+			if err = conn.WriteMessage(mt, msg); err != nil {
+				w.Errors = append(w.Errors, err)
+			}
+		}
+	}
+
 }
-func TestWebsocketMock(t *testing.T) {
-	srv := httptest.NewServer(http.HandlerFunc(websocketHandler))
+
+func NewWebsocketMock() *websocketMock {
+	mock := &websocketMock{
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	}
+
+	srv := httptest.NewServer(http.HandlerFunc(mock.upgradedHandler))
 	u, _ := url.Parse(srv.URL)
 	u.Scheme = "ws"
-	http.DefaultTransport = &mockTransport{}
 
-	client := NewWebsocketClient(u.String())
+	mock.MockURL = u.String()
+
+	return mock
+}
+
+func TestWebsocketMock(t *testing.T) {
+	mock := NewWebsocketMock()
+
+	client := NewWebsocketClient(mock.MockURL)
 
 	if err := client.Send(&model.Event{}); err != nil {
 		t.Fatal(err)
+	}
+
+	if mock.Errors != nil {
+		for _, err := range mock.Errors {
+			t.Log(err)
+		}
+		t.Fail()
 	}
 }
