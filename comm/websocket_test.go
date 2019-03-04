e43715ddc59ae017dd1fa929ad20ff1d547fa410
Pavle Jonoski <jonoski.pavle@gmail.com>
2019-03-04 22:02:27 +0100

    Websocket send unit test.

--- comm/websocket_test.go
+++ comm/websocket_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/theia-log/selene/model"
@@ -24,6 +25,7 @@ type websocketMock struct {
 	MockURL  string
 	upgrader websocket.Upgrader
 	Errors   []error
+	done     chan bool
 }
 
 func (w *websocketMock) Expect(mesage string) *websocketMock {
@@ -36,20 +38,41 @@ func (w *websocketMock) Respond(message string) *websocketMock {
 	return w
 }
 
-func (w websocketMock) upgradedHandler(resp http.ResponseWriter, req *http.Request) {
+func (w *websocketMock) AddError(err error) *websocketMock {
+	if w.Errors == nil {
+		w.Errors = []error{}
+	}
+	w.Errors = append(w.Errors, err)
+	return w
+}
+
+func (w *websocketMock) markRequestCompleted() {
+	w.done <- true
+}
+
+func (w *websocketMock) WaitRequestsToComplete(n int) {
+	for ; n > 0; n-- {
+		<-w.done
+	}
+}
+
+func (w *websocketMock) upgradedHandler(resp http.ResponseWriter, req *http.Request) {
 	conn, err := w.upgrader.Upgrade(resp, req, nil)
 	if err != nil {
 		http.Error(resp, fmt.Sprintf("cannot upgrade: %v", err), http.StatusInternalServerError)
+		w.markRequestCompleted()
+		return
 	}
 	mt, p, err := conn.ReadMessage()
 	if err != nil {
-		w.Errors = append(w.Errors, err)
+		w.AddError(err)
+		w.markRequestCompleted()
 		return
 	}
-
 	if w.expect != nil {
 		if !w.expect.EqualsTo(p) {
-			w.Errors = append(w.Errors, fmt.Errorf("expected '%s' but got '%s'", string(w.expect), string(p)))
+			w.AddError(fmt.Errorf("expected '%s' but got '%s'", string(w.expect), string(p)))
+			w.markRequestCompleted()
 			return
 		}
 	}
@@ -57,11 +80,13 @@ func (w websocketMock) upgradedHandler(resp http.ResponseWriter, req *http.Reque
 	if w.respond != nil {
 		for _, msg := range w.respond {
 			if err = conn.WriteMessage(mt, msg); err != nil {
-				w.Errors = append(w.Errors, err)
+				w.AddError(err)
+				w.markRequestCompleted()
+				return
 			}
 		}
 	}
-
+	w.markRequestCompleted()
 }
 
 func NewWebsocketMock() *websocketMock {
@@ -70,6 +95,7 @@ func NewWebsocketMock() *websocketMock {
 			ReadBufferSize:  1024,
 			WriteBufferSize: 1024,
 		},
+		done: make(chan bool, 5),
 	}
 
 	srv := httptest.NewServer(http.HandlerFunc(mock.upgradedHandler))
@@ -81,15 +107,29 @@ func NewWebsocketMock() *websocketMock {
 	return mock
 }
 
-func TestWebsocketMock(t *testing.T) {
-	mock := NewWebsocketMock()
+func TestWebsocketClientSend(t *testing.T) {
+	mock := NewWebsocketMock().Expect(strings.Join([]string{
+		"event:71 65 6",
+		"id:id-001",
+		"timestamp:1551733035.230000",
+		"source:/src",
+		"tags:tag1,tag2",
+		"event1",
+	}, "\n"))
 
 	client := NewWebsocketClient(mock.MockURL)
 
-	if err := client.Send(&model.Event{}); err != nil {
+	if err := client.Send(&model.Event{
+		ID:        "id-001",
+		Source:    "/src",
+		Timestamp: 1551733035.23,
+		Tags:      []string{"tag1", "tag2"},
+		Content:   "event1",
+	}); err != nil {
 		t.Fatal(err)
 	}
 
+	mock.WaitRequestsToComplete(1)
 	if mock.Errors != nil {
 		for _, err := range mock.Errors {
 			t.Log(err)
